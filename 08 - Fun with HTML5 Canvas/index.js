a45bfc5708f1cc43e741fbece07d85cb296ea19a
Konstantinos Melios <constantinemelios@gmail.com>
2020-09-16 17:36:16 +0300

    Day 8 - Solution

--- 08 - Fun with HTML5 Canvas/index.js
+++ 08 - Fun with HTML5 Canvas/index.js
@@ -3,7 +3,8 @@ const canvas = document.getElementById('draw')
 const context = canvas.getContext('2d')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
-context.lineWidth= 100
+//Starting width
+let radius= 70
 //Variables
 let isDrawing = false
 let lastX = 0
@@ -14,12 +15,29 @@ let direction = true
 
 function draw(event){
     if(isDrawing) {
-        context.fillStyle = 
+        context.fillStyle = `hsl(${hue}, 100%, 50%)`
         context.beginPath()
         context.moveTo(event.x, event.y)
-        context.arc(event.x, event.y, context.lineWidth, 0, 360)
+        context.arc(event.x, event.y, radius, 0, 360)
         context.fill()
+
+        hue++
+        if(hue> 360) {
+            hue = 0
+        }
+        if(radius < 20) {
+            direction = false
+        } else if (radius > 70) {
+            direction =true
+        }
+        if(direction){
+            radius = radius - 0.1
+        } else {
+            radius = radius + 0.1
+        }
+        console.log("decreasing:", direction, "width:", radius)
     }
+    return
 }
 
 canvas.addEventListener('mousemove', draw)
